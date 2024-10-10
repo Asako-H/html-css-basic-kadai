c96a9a7b62f3d63bb171671913bb993e050651ea
Asako-H <hoshi_asako@outlook.jp>
2024-10-10 15:50:18 +0900

    upload files2

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,19 +1,19 @@
 // 1以上の整数を代入する
-let num = 15;
+let num = 30;
 // 変数numの値を出力する
 console.log(num);
 
-// 変数の値が3の倍数
-if (num % 3 === 0) {
-  console.log("3の倍数です");
-}
-// 変数の値が5の倍数
-if (num % 5 === 0) {
-  console.log("5の倍数です");
-}
 // 変数の値が3と5の倍数
 if (num % 3 === 0 && num % 5 === 0) {
   console.log("3と5の倍数です");
+}
+// 変数の値が3の倍数
+else if (num % 3 === 0) {
+  console.log("3の倍数です");
+}
+// 変数の値が5の倍数
+else if (num % 5 === 0) {
+  console.log("5の倍数です");
 }
 //
 else {
